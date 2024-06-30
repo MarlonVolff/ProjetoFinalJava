3ccd90c6527fb29fd33d65496f4c29db4b79de29
Lucas G. Lisboa <lglisboa1503@gmail.com>
2024-06-30 15:39:54 -0300

    models/Livro.java[100%]

---
+++ src/models/Livros.java
@@ -0,0 +1,49 @@
+public class Livros {
+    private int id;
+    private String titulo;
+    private String autor;
+    private String isbn;
+    
+    public Livros() {
+    }
+    public Livros(int id) {
+        this.id = id;
+    }
+    public Livros(int id, String titulo, String autor, String isbn) {
+        this.id = id;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.isbn = isbn;
+    }
+    
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getTitulo() {
+        return titulo;
+    }
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+    public String getAutor() {
+        return autor;
+    }
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+    public String getIsbn() {
+        return isbn;
+    }
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    @Override
+    public String toString() {
+        return "Livros [id=" + id + ", titulo=" + titulo + ", autor=" + autor + ", isbn=" + isbn + "]";
+    }
+    
+}
